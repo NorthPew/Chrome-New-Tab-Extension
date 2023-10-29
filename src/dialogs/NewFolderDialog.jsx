2afe17ddcaa194c4c8c8e19409cdd03aea7f5d73
Victor (Vigge) B <vbergstrom02@gmail.com>
2023-10-29 18:35:36 +0100

    Now it will make a new object with the title of the input text and paste it in localstorage

--- src/dialogs/NewFolderDialog.jsx
+++ src/dialogs/NewFolderDialog.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { ExtensionContext } from "../ContextRoot"
 
 const Dialog = styled.dialog`
@@ -12,14 +12,44 @@ export function NewFolderDialog() {
 
     const {stateNewFolderDialog, newFolderDialogRef} = useContext(ExtensionContext)
 
+    const [newFolderTitle, setNewFolderTitle] = useState("")
+
+    function handleOnChangeNewFolderTitle(e) {
+        setNewFolderTitle(e.target.value)
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+
+        if (newFolderTitle !== "") {
+
+            console.log("Creating a new empty folder. Title: ", newFolderTitle);
+
+            let parseTemplate = JSON.parse(localStorage.getItem("Template"))
+
+            let newEmptyFolder = {
+                id: parseTemplate.folders.length + 1,
+                title: newFolderTitle,
+                contents: []
+            }
+
+            console.log("New Folder: ", newEmptyFolder);
+
+            parseTemplate.folders.push(newEmptyFolder)
+
+            localStorage.setItem("Template", JSON.stringify(parseTemplate))
+        } 
+    }
+
     return (
     <Dialog ref={newFolderDialogRef}>
         <button onClick={() => stateNewFolderDialog(false)}>Exit</button>
-        <h1>Add new folder entry</h1>
-        <label htmlFor="new-folder-name-input">
-            Folder Name: 
-        </label>
-        <input type="text" id="new-folder-name-input" placeholder="😎 Socials"></input>
+        <form method="dialog" onSubmit={handleSubmit}>
+            <h1>Add new folder entry</h1>
+            <label htmlFor="new-folder-name-input">Folder Title: </label>
+            <input type="text" id="new-folder-name-input" placeholder="😎 Socials" onChange={handleOnChangeNewFolderTitle} value={newFolderTitle}></input>
+            <button type="submit" onClick={() => stateNewFolderDialog(false)}>Create</button>
+        </form>
     </Dialog>
     )
 }
